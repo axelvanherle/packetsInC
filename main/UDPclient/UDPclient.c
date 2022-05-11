c02e9101a2d59240e8155038b341a591d86c6acb
Axel Vanherle <axelvanherle@outlook.com>
2022-05-11 14:38:35 +0200

    Created boilerplate

---
+++ main/UDPclient/UDPclient.c
@@ -0,0 +1,30 @@
+#ifdef _WIN32
+	#define _WIN32_WINNT _WIN32_WINNT_WIN7 //select minimal legacy support, needed for inet_pton, inet_ntop
+	#include <winsock2.h> //for all socket programming
+	#include <ws2tcpip.h> //for getaddrinfo, inet_pton, inet_ntop
+	#include <stdio.h> //for fprintf
+	#include <unistd.h> //for close
+	#include <stdlib.h> //for exit
+	#include <string.h> //for memset
+#else
+	#include <sys/socket.h> //for sockaddr, socket, socket
+	#include <sys/types.h> //for size_t
+	#include <netdb.h> //for getaddrinfo
+	#include <netinet/in.h> //for sockaddr_in
+	#include <arpa/inet.h> //for htons, htonl, inet_pton, inet_ntop
+	#include <errno.h> //for errno
+	#include <stdio.h> //for fprintf, perror
+	#include <unistd.h> //for close
+	#include <stdlib.h> //for exit
+	#include <string.h> //for memset
+#endif
+
+int main()
+{
+    WSADATA wsaData;
+	WSAStartup( MAKEWORD(2,0), &wsaData );
+
+    WSACleanup();
+    printf("Compiler works!\n");
+    return 0;
+}
