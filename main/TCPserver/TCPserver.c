065ba4765928a421f67807cdc63f9e3720a15bdc
Axel Vanherle <axelvanherle@outlook.com>
2022-05-19 23:58:03 +0200

    FUCK TCP!

--- main/TCPserver/TCPserver.c
+++ main/TCPserver/TCPserver.c
@@ -36,221 +36,157 @@
 	void OSCleanup( void ) {}
 #endif
 
-#define PORT "9034"   // Port we're listening on
+int initialization();
+int connection( int internet_socket );
+void execution( int internet_socket );
+void cleanup( int internet_socket, int client_internet_socket );
 
-// Get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa);
-
-// Return a listening socket
-int get_listener_socket(void);
-
-// Add a new file descriptor to the set
-void add_to_pfds(struct pollfd *pfds[], int newfd, int *fd_count, int *fd_size);
-
-// Remove an index from the set
-void del_from_pfds(struct pollfd pfds[], int i, int *fd_count);
-
-// Main
-int main(void)
+int main( int argc, char * argv[] )
 {
+	//////////////////
+	//Initialization//
+	//////////////////
+
 	OSInit();
-    int listener;     // Listening socket descriptor
 
-    int newfd;        // Newly accept()ed socket descriptor
-    struct sockaddr_storage remoteaddr; // Client address
-    socklen_t addrlen;
+	int internet_socket = initialization();
 
-    char buf[1000];    // Buffer for client data
+	//////////////
+	//Connection//
+	//////////////
 
-    char remoteIP[INET6_ADDRSTRLEN];
+	int client_internet_socket = connection( internet_socket );
 
-    // Start off with room for 5 connections
-    // (We'll realloc as necessary)
-    int fd_count = 0;
-    int fd_size = 5;
-    struct pollfd *pfds = malloc(sizeof *pfds * fd_size);
+	/////////////
+	//Execution//
+	/////////////
 
-    // Set up and get a listening socket
-    listener = get_listener_socket();
-
-    if (listener == -1) {
-        fprintf(stderr, "error getting listening socket\n");
-        exit(1);
+    while (1)
+    {
+        execution( client_internet_socket );
     }
 
-    // Add the listener to set
-    pfds[0].fd = listener;
-    pfds[0].events = POLLIN; // Report ready to read on incoming connection
+	cleanup( internet_socket, client_internet_socket );
 
-    fd_count = 1; // For the listener
-
-    // Main loop
-    for(;;) {
-        int poll_count = WSAPoll(pfds, fd_count, -1);
-
-        if (poll_count == -1) {
-            perror("poll");
-            exit(1);
-        }
-
-        // Run through the existing connections looking for data to read
-        for(int i = 0; i < fd_count; i++) {
-
-            // Check if someone's ready to read
-            if (pfds[i].revents & POLLIN) { // We got one!!
-
-                if (pfds[i].fd == listener) {
-                    // If listener is ready to read, handle new connection
-
-                    addrlen = sizeof remoteaddr;
-                    newfd = accept(listener,
-                        (struct sockaddr *)&remoteaddr,
-                        &addrlen);
-
-                    if (newfd == -1) {
-                        perror("accept");
-                    } else {
-                        add_to_pfds(&pfds, newfd, &fd_count, &fd_size);
-
-                        printf("pollserver: new connection from %s on "
-                            "socket %d\n",
-                            inet_ntop(remoteaddr.ss_family,
-                                get_in_addr((struct sockaddr*)&remoteaddr),
-                                remoteIP, INET6_ADDRSTRLEN),
-                            newfd);
-                    }
-                } else {
-                    // If not the listener, we're just a regular client
-                    int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
-
-                    int sender_fd = pfds[i].fd;
-
-                    if (nbytes <= 0) {
-                        // Got error or connection closed by client
-                        if (nbytes == 0) {
-                            // Connection closed
-                            printf("pollserver: socket %d hung up\n", sender_fd);
-                        } else {
-                            perror("recv");
-                        }
-
-                        close(pfds[i].fd); // Bye!
-
-                        del_from_pfds(pfds, i, &fd_count);
-
-                    } else {
-                        // We got some good data from a client
-  
-                        for(int j = 0; j < fd_count; j++) {
-                            if (j == 0)
-                            {    
-                                printf("Received a message\n");
-                            }
-
-                            // Send to everyone!
-                            int dest_fd = pfds[j].fd;
-
-                            // Except the listener and ourselves
-                            if (dest_fd != listener && dest_fd != sender_fd) {
-                                if (send(dest_fd, buf, nbytes, 0) == -1) 
-                                {   
-                                    printf("TEST, %s\n",buf);
-                                    perror("send");
-                                }
-                            }
-                        }
-                    }
-                } // END handle data from client
-            } // END got ready-to-read from poll()
-        } // END looping through file descriptors
-    } // END for(;;)--and you thought it would never end!
-    
 	OSCleanup();
-    return 0;
+
+	return 0;
 }
 
-// Get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+int initialization()
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+	//Step 1.1
+	struct addrinfo internet_address_setup;
+	struct addrinfo * internet_address_result;
+	memset( &internet_address_setup, 0, sizeof internet_address_setup );
+	internet_address_setup.ai_family = AF_UNSPEC;
+	internet_address_setup.ai_socktype = SOCK_STREAM;
+	internet_address_setup.ai_flags = AI_PASSIVE;
+	int getaddrinfo_return = getaddrinfo( NULL, "24042", &internet_address_setup, &internet_address_result );
+	if( getaddrinfo_return != 0 )
+	{
+		fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+		exit( 1 );
+	}
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	int internet_socket = -1;
+	struct addrinfo * internet_address_result_iterator = internet_address_result;
+	while( internet_address_result_iterator != NULL )
+	{
+		//Step 1.2
+		internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
+		if( internet_socket == -1 )
+		{
+			perror( "socket" );
+		}
+		else
+		{
+			//Step 1.3
+			int bind_return = bind( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+			if( bind_return == -1 )
+			{
+				perror( "bind" );
+				close( internet_socket );
+			}
+			else
+			{
+				//Step 1.4
+				int listen_return = listen( internet_socket, 1 );
+				if( listen_return == -1 )
+				{
+					close( internet_socket );
+					perror( "listen" );
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		internet_address_result_iterator = internet_address_result_iterator->ai_next;
+	}
+
+	freeaddrinfo( internet_address_result );
+
+	if( internet_socket == -1 )
+	{
+		fprintf( stderr, "socket: no valid socket address found\n" );
+		exit( 2 );
+	}
+
+	return internet_socket;
 }
 
-// Return a listening socket
-int get_listener_socket(void)
+int connection( int internet_socket )
 {
-    int listener;     // Listening socket descriptor
-    int yes=1;        // For setsockopt() SO_REUSEADDR, below
-    int rv;
-
-    struct addrinfo hints, *ai, *p;
-
-    // Get us a socket and bind it
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
-        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
-        exit(1);
-    }
-    
-    for(p = ai; p != NULL; p = p->ai_next) {
-        listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (listener < 0) { 
-            continue;
-        }
-        
-        // Lose the pesky "address already in use" error message
-        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-        if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
-            close(listener);
-            continue;
-        }
-
-        break;
-    }
-
-    freeaddrinfo(ai); // All done with this
-
-    // If we got here, it means we didn't get bound
-    if (p == NULL) {
-        return -1;
-    }
-
-    // Listen
-    if (listen(listener, 10) == -1) {
-        return -1;
-    }
-
-    return listener;
+	//Step 2.1
+	struct sockaddr_storage client_internet_address;
+	socklen_t client_internet_address_length = sizeof client_internet_address;
+	int client_socket = accept( internet_socket, (struct sockaddr *) &client_internet_address, &client_internet_address_length );
+	if( client_socket == -1 )
+	{
+		perror( "accept" );
+		close( internet_socket );
+		exit( 3 );
+	}
+	return client_socket;
 }
 
-// Add a new file descriptor to the set
-void add_to_pfds(struct pollfd *pfds[], int newfd, int *fd_count, int *fd_size)
+void execution( int internet_socket )
 {
-    // If we don't have room, add more space in the pfds array
-    if (*fd_count == *fd_size) {
-        *fd_size *= 2; // Double it
+	//Step 3.1
+	int number_of_bytes_received = 0;
+	char buffer[1000];
+	number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
+	if( number_of_bytes_received == -1 )
+	{
+		perror( "recv" );
+	}
+	else
+	{
+		buffer[number_of_bytes_received] = '\0';
+		printf( "Received : %s\n", buffer );
+	}
 
-        *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
-    }
-
-    (*pfds)[*fd_count].fd = newfd;
-    (*pfds)[*fd_count].events = POLLIN; // Check ready-to-read
-
-    (*fd_count)++;
+	//Step 3.2
+	int number_of_bytes_send = 0;
+	number_of_bytes_send = send( internet_socket, "Hello TCP world!", 16, 0 );
+	if( number_of_bytes_send == -1 )
+	{
+		perror( "send" );
+	}
 }
 
-// Remove an index from the set
-void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
+void cleanup( int internet_socket, int client_internet_socket )
 {
-    // Copy the one from the end over this one
-    pfds[i] = pfds[*fd_count-1];
+	//Step 4.2
+	int shutdown_return = shutdown( client_internet_socket, SD_RECEIVE );
+	if( shutdown_return == -1 )
+	{
+		perror( "shutdown" );
+	}
 
-    (*fd_count)--;
+	//Step 4.1
+	close( client_internet_socket );
+	close( internet_socket );
 }
