23324f04bfa1ec4bbf34b2295fe27f27cceb971d
Axel Vanherle <axelvanherle@outlook.com>
2022-05-25 23:37:34 +0200

    Ik ga sllapen

--- main/test.c
+++ main/test.c
@@ -4,17 +4,26 @@
 
 int main()
 {
-	char str[] = "232.75048, 3,  0.364, -0.278, 9.443, 4, -1.222, 0.001, -6.664, 5, -465.654, -654.654, 150.644";
-	double gyro[20];
-	unsigned int whatever[4];
+	char buffer[] = "232.75048, 3,  0.364, -0.278, 9.443, 4, -1.222, 0.001, -6.664, 5, -465.654, -654.654, 150.644";
 	double trash;
+	double stats1[3];
+	double stats2[3];
+	double stats3[3];
 
+	sscanf(buffer, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",&trash,&trash,&stats1[0],&stats1[1],&stats1[2],&trash,&stats2[0],&stats2[1],&stats2[2],&trash,&stats3[0],&stats3[1],&stats3[2]);
 
-	sscanf(str, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf",&trash,&trash,&gyro[0],&gyro[1],&gyro[2],&trash,&gyro[3],&gyro[4],&gyro[5],&trash,&gyro[6],&gyro[7],&gyro[8]);
-
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		printf("%lf\n",gyro[i]);
+		printf("%lf\n",stats1[i]);
 	}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%lf\n",stats2[i]);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%lf\n",stats3[i]);
+	}
+		
 	
 }
